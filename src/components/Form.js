9010706946f74af4e59144f4ea42ed636ae75712
Michael Callahan <CallahanMichaelE@gmail.com>
2020-02-27 16:21:19 -0700

    mvp hit

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,14 @@
-import React from "react"
+import React, { useState, useEffect} from "react"
 import { withFormik, Form, Field} from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 
 const FormOnBoard = ({ values, touched, errors, status })=>{
-
+    const [person, setPerson] = useState([])
+    useEffect(()=> {
+        console.log("status has changed", status);
+        status && setPerson(person => [...person, status]);
+    }, [status])
     return (
         <div>
             <Form>
@@ -56,6 +60,13 @@ const FormOnBoard = ({ values, touched, errors, status })=>{
                 </label>
                 <br/>
                 <button type="submit">Submit</button>
+                <pre>{JSON.stringify(values, null, 2)}</pre>
+                {person.map(user => (
+                    <ul key={user.id}>
+                        <li>Name: {user.name}</li>
+                        <li>Email: {user.email}</li>
+                    </ul>
+                ))}
             </Form>
         </div>
     )
@@ -72,10 +83,18 @@ const FormikForm = withFormik({
     },
     validationSchema: Yup.object().shape({
         name: Yup.string().required(),
-        email: Yup.string().required(),
-        password: Yup.string().required(),
-        tos: Yup.string().required()
+        email: Yup.string().required().email("Must be a valid email address"),
+        password: Yup.string().required().min(8, "Password must be 8 characters"),
+        tos: Yup.boolean().oneOf([true], 'Must Accept Terms and Conditions')
     }),
+    handleSubmit(values, {setStatus, resetForm}){
+        console.log("submitting", values);
+        axios.post('https://reqres.in/api/users', values).then(response => {
+        console.log("success", response);
+        setStatus(response.data);
+        resetForm();
+        })
+    }
 })(FormOnBoard);
 
 export default FormikForm
